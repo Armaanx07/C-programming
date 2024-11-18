43ab0a54332e9b90edffba92c7b036a412cdbba1
Armaanx07 <armaan07@eng.rizvi.edu.in>
2024-11-18 15:39:05 +0530

    exp1 task1.c

---
+++ exp1 task1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    printf("name\t:Armaan Shaikh\n");
+    printf("Age\t:19\n");
+    printf("class\t:F.E\n");
+    printf("division:A\n");
+    printf("UIN\t:A1066");
+
+}
